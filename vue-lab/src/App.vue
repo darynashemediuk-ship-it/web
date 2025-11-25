1075721490ab797d94bc1d3b2873d58cf4928d37
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-11-25 11:27:47 +0200

    refactor(App): Refactor App.vue layout and apply Tailwind CSS for navigation

--- vue-lab/src/App.vue
+++ vue-lab/src/App.vue
@@ -1,23 +1,20 @@
-<script setup></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="p-8 bg-white rounded-xl shadow-lg hover:shadow-2xl transition max-w-sm">
-      <h1 class="text-3xl font-bold text-indigo-600 mb-4">
-        Vue 3 + Tailwind CSS v4
-      </h1>
+  <header>
+    <div class="max-w-4xl mx-auto">
+      <nav class="p-4 bg-gray-100 rounded-b-lg shadow-md">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-        <p class="text-gray-600">
-          Вітаю! Tailwind CSS v4 успішно працює у вашому проєкті.
-        </p>
-
-        <div class="flex justify-center md:justify-end">
-          <button class="px-6 py-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition w-full md:w-auto">
-            Натисни мене
-          </button>
-        </div>
-      </div>
+      </nav>
     </div>
-  </div>
+  </header>
+
+  <main class="max-w-4xl mx-auto mt-5">
+    <RouterView />
+  </main>
 </template>

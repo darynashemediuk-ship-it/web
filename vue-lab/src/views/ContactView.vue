def3b4fdc8306bc4081b3df560581e8c7f370732
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-11-26 15:14:17 +0200

    feat: створення компонента ContactView та конфігурація контенту

--- vue-lab/src/views/ContactView.vue
+++ vue-lab/src/views/ContactView.vue
@@ -1,17 +1,104 @@
-<script setup lang="ts">
-import UserCard from '@/components/UserCard.vue';
-</script>
-
-
 <template>
-  <div class="p-5">
-    <h1 class="text-2xl font-bold mb-4">Сторінка контактів</h1>
-    <p>Це наша нова сторінка контактів!</p>
+  <div class="splash-wrapper">
 
-    <div class="user-cards-row">
-      <UserCard username="Student_1" :isActive="true" />
-      <UserCard username="Student_2" :isActive="false" />
+    <div class="illustration-container">
+      <img :src="currentContent.imageSrc" :alt="currentContent.imageAlt">
     </div>
 
+    <div class="text-content-container">
+
+      <button class="main-action-button">{{ currentContent.buttonText }}</button>
+
+      <p class="description-text">
+        {{ currentContent.descriptionText }}
+      </p>
+    </div>
+
+    <div class="bottom-nav">
+      <RouterLink to="/home" class="nav-button nav-skip">
+        SKIP
+      </RouterLink>
+
+      <div class="pagination-dots">
+        <div
+          v-for="i in 3"
+          :key="i"
+          class="dot"
+          :class="{ 'active': i === currentPage.id }"
+          @click="changePage(i)"
+        ></div>
+      </div>
+
+      <button
+        class="nav-button nav-next"
+        @click="nextPage"
+      >
+        NEXT
+      </button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+
+const pageContents = [
+  {
+    id: 1,
+    imageSrc: 'photo1.svg',
+    imageAlt: 'Ілюстрація для знайомств',
+    buttonText: 'MEET NEW PEOPLE',
+    descriptionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    id: 2,
+    imageSrc: 'photo2.svg',
+    imageAlt: 'Ілюстрація для чату з легкістю',
+    buttonText: 'CHAT WITH EASE',
+    descriptionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    id: 3,
+    imageSrc: 'photo3.svg',
+    imageAlt: 'Ілюстрація для знаходження спорідненої душі',
+    buttonText: 'FIND YOUR SOULMATE',
+    descriptionText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  }
+];
+
+const currentPageId = ref(1);
+const router = useRouter();
+
+const currentPage = computed(() => {
+  return pageContents.find(p => p.id === currentPageId.value) || pageContents[0];
+});
+
+const changePage = (id: number) => {
+  if (id >= 1 && id <= 3) {
+    currentPageId.value = id;
+  }
+};
+
+const nextPage = () => {
+  if (currentPageId.value < 3) {
+    currentPageId.value++;
+  } else {
+    router.push('/home');
+  }
+};
+
+const currentContent = computed(() => {
+  return pageContents.find(p => p.id === currentPageId.value) || pageContents[0];
+});
+</script>
+
+<style scoped>
+.nav-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+}
+
+</style>

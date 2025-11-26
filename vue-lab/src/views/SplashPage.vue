0424b7b0eb5b49345d2285b0a9e4218c2d05d934
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-11-26 13:40:15 +0200

    feat: Налаштовано маршрути /splash/1, /splash/2, /splash/3 з динамічними пропсами

--- vue-lab/src/views/SplashPage.vue
+++ vue-lab/src/views/SplashPage.vue
@@ -58,7 +58,7 @@ const props = defineProps<{
   imageSrc: string;
   imageAlt: string;
   buttonText: string;
-  currentPage: number; // 1, 2, або 3
+  currentPage: number;
   descriptionText: string;
 }>();
 </script>

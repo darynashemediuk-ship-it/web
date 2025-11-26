3b86c4e65c44e8209a1d396f8fa4e5448451c806
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-11-26 15:15:14 +0200

    feat: видалення зайвих сторінок з головної сторінки

--- vue-lab/src/App.vue
+++ vue-lab/src/App.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { computed } from 'vue';
+
+const router = useRouter();
+
+const isContactPage = computed(() => {
+  return router.currentRoute.value.path === '/contact';
+});
 </script>
 
 <template>
-  <header>
+
+  <header v-if="!isContactPage">
     <div class="max-w-4xl mx-auto">
       <nav class="p-4 bg-gray-100 rounded-b-lg shadow-md">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-
+        <RouterLink to="/contact">Dating App</RouterLink>
       </nav>
     </div>
   </header>
 
-  <main class="max-w-4xl mx-auto mt-5">
+  <main :class="{'max-w-4xl mx-auto mt-5': !isContactPage}">
     <RouterView />
   </main>
 </template>

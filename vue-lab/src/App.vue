db2f2309aad43efaffb036d7056d59fbd4933487
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-12-03 18:38:46 +0200

    feat: оновлено сторінку App.vue та index.ts

--- vue-lab/src/App.vue
+++ vue-lab/src/App.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
 import { computed } from 'vue';
 
 const router = useRouter();
 
-const isContactPage = computed(() => {
-  return router.currentRoute.value.path === '/contact';
+const isAuthPage = computed(() => {
+  const path = router.currentRoute.value.path;
+  return path === '/login' || path === '/register';
+});
+
+const isStandardPage = computed(() => {
+  return !isAuthPage.value;
 });
 </script>
 
 <template>
-
-  <header v-if="!isContactPage">
-    <div class="max-w-4xl mx-auto">
-      <nav class="p-4 bg-gray-100 rounded-b-lg shadow-md">
-        <RouterLink to="/contact">Dating App</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <main :class="{'max-w-4xl mx-auto mt-5': !isContactPage}">
+  <main :class="{'auth-layout': isAuthPage, 'max-w-6xl mx-auto mt-5': isStandardPage}">
     <RouterView />
   </main>
 </template>
+
+<style>
+
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.auth-layout {
+  min-height: 100vh;
+  width: 100%;
+  background-color: #f4625b;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>

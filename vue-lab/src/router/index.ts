2cdd9c90cf13568fca5d33694679525c3321693b
Daryna Shemediuk <daryna.shemediuk@oa.edu.ua>
2025-12-03 18:39:01 +0200

    feat: оновлено сторінку App.vue та index.ts

--- vue-lab/src/router/index.ts
+++ vue-lab/src/router/index.ts
@@ -4,21 +4,28 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
+
     {
       path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
+      name: 'splash',
+      component: () => import('../views/SplashPage.vue')
     },
+
+
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterPage.vue')
     },
+
+
     {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('../views/ContactView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginPage.vue')
     }
+
+
   ],
 })
 
